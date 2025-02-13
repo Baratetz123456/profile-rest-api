4eefd13837edf1a5ef979f798fe54ab49d68ab57
Jayson Barateta <jayson.b@nova-hub.net>
2025-02-13 16:10:24 +0800

    Added profiles viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,8 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -91,8 +94,17 @@ class HelloViewSet(viewsets.ViewSet):
     
     def partial_update(self, request, pk=None):
         """Handle updating part of an object"""
-        return Response({'httpd_method': 'PATCH'})
+        return Response({'http_method': 'PATCH'})
     
     def destroy(self, request, pk=None):
         """Handle removing an object"""
-        return Response({'httpd_method': 'DELETE'})
+        return Response({'http_method': 'DELETE'})
+    
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authencation_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    
